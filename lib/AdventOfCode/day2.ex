af4069dd8ec2508d47cd91e5e4eccdb0e2f799bd
mighty_peach <mighty_peach@fastmail.com>
2024-12-02 20:30:00 +0100

    day 2_2

--- lib/AdventOfCode/day2.ex
+++ lib/AdventOfCode/day2.ex
@@ -1,10 +1,6 @@
 defmodule AdventOfCode.Day2 do
   import AdventOfCode.FileHandler
 
-  @moduledoc """
-  Only first_task is correct ;(
-  """
-
   def first_task do
     result =
       read_input("2_1.txt")
@@ -18,8 +14,6 @@ defmodule AdventOfCode.Day2 do
     read_input("2_1.txt")
     |> prepare_data
     |> check_with_dampener
-
-    # IO.inspect(check_with_dampener([[4, 3, 1, 2]]))
   end
 
   def check([head | tail], sum) do
@@ -34,69 +28,21 @@ defmodule AdventOfCode.Day2 do
   def check(list), do: check(list, 0)
 
   def check_with_dampener([head | tail], sum) do
-    safe = is_safe_tolerated?(head)
-
-    IO.puts("#{inspect(head, charlists: :as_lists)} - #{is_safe_tolerated?(head)}")
+    safe = is_safe?(head)
 
     if safe do
       check_with_dampener(tail, sum + 1)
     else
-      check_with_dampener(tail, sum)
+      if Enum.any?(get_variants(head), &is_safe?/1) do
+        check_with_dampener(tail, sum + 1)
+      else
+        check_with_dampener(tail, sum)
+      end
     end
   end
 
   def check_with_dampener([], sum), do: sum
   def check_with_dampener(list), do: check_with_dampener(list, 0)
-
-  def is_safe_tolerated?(list) do
-    all_increasing_tolerated?(list) || all_decreasing_tolerated?(list)
-  end
-
-  def all_decreasing_tolerated?([x | tail] = list, is_dampener_used) when length(list) > 1 do
-    [y | t] = tail
-
-    z =
-      if is_list(t) do
-        List.first(t, nil)
-      else
-        nil
-      end
-
-    cond do
-      decreasing(x, y) -> all_decreasing_tolerated?(tail, is_dampener_used)
-      !is_dampener_used and is_nil(z) -> true
-      is_nil(z) -> false
-      decreasing(x, z) and !is_dampener_used -> all_decreasing_tolerated?(t, true)
-      decreasing(y, z) and !is_dampener_used -> all_decreasing_tolerated?(t, true)
-      true -> false
-    end
-  end
-
-  def all_decreasing_tolerated?(list, _) when length(list) <= 1, do: true
-  def all_decreasing_tolerated?(list), do: all_decreasing_tolerated?(list, false)
-
-  def all_increasing_tolerated?([x | tail] = list, is_dampener_used) when length(list) > 1 do
-    [y | t] = tail
-
-    z =
-      if is_list(t) do
-        List.first(t, nil)
-      else
-        nil
-      end
-
-    cond do
-      increasing(x, y) -> all_increasing_tolerated?(tail, is_dampener_used)
-      !is_dampener_used and is_nil(z) -> true
-      is_nil(z) -> false
-      increasing(x, z) and !is_dampener_used -> all_increasing_tolerated?(t, true)
-      increasing(y, z) and !is_dampener_used -> all_increasing_tolerated?(t, true)
-      true -> false
-    end
-  end
-
-  def all_increasing_tolerated?(list, _) when length(list) <= 1, do: true
-  def all_increasing_tolerated?(list), do: all_increasing_tolerated?(list, false)
 
   def is_safe?(list) do
     all_increasing?(list) || all_decreasing?(list)
@@ -105,7 +51,7 @@ defmodule AdventOfCode.Day2 do
   def all_increasing?([x | tail] = list) when length(list) > 1 do
     y = List.first(tail)
 
-    if increasing(x, y) <= 3 do
+    if x < y and y - x <= 3 do
       all_increasing?(tail)
     else
       false
@@ -117,7 +63,7 @@ defmodule AdventOfCode.Day2 do
   def all_decreasing?([x | tail] = list) when length(list) > 1 do
     y = List.first(tail)
 
-    if decreasing(x, y) <= 3 do
+    if x > y and x - y <= 3 do
       all_decreasing?(tail)
     else
       false
@@ -143,11 +89,9 @@ defmodule AdventOfCode.Day2 do
     end)
   end
 
-  def decreasing(x, y) do
-    x > y and x - y <= 3
-  end
-
-  def increasing(x, y) do
-    x < y and y - x <= 3
+  def get_variants(list) do
+    Enum.map(0..(length(list) - 1), fn x ->
+      List.delete_at(list, x)
+    end)
   end
 end
